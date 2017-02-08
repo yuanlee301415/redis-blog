5109711020083c8b9b685c160e0c78d4c1730ad2
lee yuan <liyuan520310@163.com>
2017-02-08 17:29:23 +0800

    comment v1

--- routes/article.js
+++ routes/article.js
@@ -5,6 +5,7 @@ var config=require('../config');
 var cli=require('redis').createClient({db:1});
 var Ep=require('eventproxy');
 var async=require('async');
+var moment=require('moment');
 var uuid=require('uuid/v4');
 
 module.exports = router;
@@ -12,13 +13,34 @@ module.exports = router;
 //博客详细
 router.get('/:postId', (req,res,next)=>{
     var key='posts:'+req.params.postId;
-    cli.hgetall(key,(err,ret)=>{
-        if(err)return next(err);
-        if(!ret)return req.flash('error','文章不存在或已经删除！'),res.redirect('/notify');
-        //console.log('getPost>ret:',key,ret);
-        ret.tags=ret.tags.legnth?ret.tags.split(','):[];
-        //console.log('getPost>ret2:',key,ret);
 
+    async.waterfall([
+        (cb)=>{
+            cli.hgetall(key,(err,ret)=>{
+                if(err)return next(err);
+                if(!ret)return req.flash('error','文章不存在或已经删除！'),res.redirect('/notify');
+                ret.tags=ret.tags.legnth?ret.tags.split(','):[];
+                cb(null,ret);
+            });
+        },
+        (post,cb)=>{
+           cli.zrange('commentIds:'+post.id,0,-1,(err,commentIds)=>{
+               if(err)return cb(err);
+               console.log('commentIds:',commentIds);
+               cb(null,post,commentIds);
+           });
+        },
+        (post,commentIds,cb)=>{
+            async.map(commentIds,(id,mcb)=>{
+                cli.hmget('comments:'+id,(err,comment)=>{
+                    mcb(null,comment);
+                });
+            },(err,comments)=>{
+                
+            });
+        }
+    ],(err)=>{
+        if(err)return next(err);
         res.render('article',{
             title:'博客详细页',
             post:ret,
@@ -30,15 +52,18 @@ router.get('/:postId', (req,res,next)=>{
         });
         cli.hincrby(key,'pv',1);
     });
-
 });
 
 //保存评论
 router.post('/:postId',checkLogin, function (req,res,next) {
-    var postId=req.params.postId,content=req.body.content.trim(),ep=new Ep();
+    var postId=req.params.postId,content=req.body.content.trim(),ep=new Ep(),user=req.session.user;
     ep.fail(next);
     ep.on('comment_err',(msg)=>{
         req.flash('error',msg);
+        res.redirect('back');
+    });
+    ep.on('send',(msg)=>{
+        req.flash('success',msg);
         res.redirect('back');
     });
     if(!content.length){
@@ -47,22 +72,25 @@ router.post('/:postId',checkLogin, function (req,res,next) {
 
     cli.hget('posts:'+postId,'id',(err,ret)=>{
         if(err)return next(err);
-        if(!ret)return ep.emit('comment-err','文章不存在或已经删除');
-        //save();
-    });
-
-    function save(){
-        var cId=uuid(),date=new Date();
-        async.parallel({
-            commentId:(cb)=>{
-                cli.zadd('comments:'+postId)
+        if(!ret)return ep.emit('comment_err','文章不存在或已经删除');
+        //save
+        var id=uuid(),date=new Date(),time=moment(date).format('YYYY-MM-DD HH:mm:ss');
+        async.parallel([
+            (cb)=>{//评论数据
+                cli.hmset('comments:'+id,['comment',content,'userName',user.name,'userId',user.id,'time',time],(err)=>{
+                    if(err)return cb(err);
+                    cb();
+                });
+            },
+            (cb)=>{//以时间降序保存评论ID
+                cli.zadd('commentsIds:'+postId,[+date,id],(err)=>{
+                    if(err)return cb(err);
+                    cb();
+                });
             }
-        },(err,ret)=>{
-
+        ],(err)=>{
+            if(err)return next(err);
+            ep.emit('send','文章评论成功！');
         });
-
-
-    }
-
-
+    });
 });
