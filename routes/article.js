2dd7f0f42d019bb89877a2484bb1b15a3bc8ae5c
lee yuan <liyuan520310@163.com>
2017-02-09 16:18:05 +0800

    comment v2

--- routes/article.js
+++ routes/article.js
@@ -16,36 +16,58 @@ router.get('/:postId', (req,res,next)=>{
 
     async.waterfall([
         (cb)=>{
-            cli.hgetall(key,(err,ret)=>{
+            cli.hgetall(key,(err,post)=>{
+                //console.log('post:',err,post);
                 if(err)return next(err);
-                if(!ret)return req.flash('error','文章不存在或已经删除！'),res.redirect('/notify');
-                ret.tags=ret.tags.legnth?ret.tags.split(','):[];
-                cb(null,ret);
+                if(!post)return req.flash('error','文章不存在或已经删除！'),res.redirect('/notify');
+                post.tags=post.tags.legnth?ret.tags.split(','):[];
+                cb(null,post);
             });
         },
         (post,cb)=>{
-           cli.zrange('commentIds:'+post.id,0,-1,(err,commentIds)=>{
+           cli.zrange('postCommentIds:'+post.id,0,-1,(err,commentIds)=>{
                if(err)return cb(err);
-               console.log('commentIds:',commentIds);
+               //console.log('commentIds:',commentIds);
                cb(null,post,commentIds);
            });
         },
         (post,commentIds,cb)=>{
             async.map(commentIds,(id,mcb)=>{
-                cli.hmget('comments:'+id,(err,comment)=>{
+                async.waterfall([
+                    (ccb)=>{
+                        cli.hgetall('comments:'+id,(err,comment)=>{
+                            //console.log('comment:',err,comment);
+                            if(err || !comment)return mcb();
+                            ccb(null,comment);
+                        });
+                    },
+                    (comment,ccb)=>{
+                        cli.hgetall('users:'+comment.userName,(err,user)=>{
+                            //console.log('user:',err,user);
+                            if(err || !user)return mcb();
+                            comment.user=user;
+                            ccb(null,comment);
+                        });
+                    }
+                ],(err,comment)=>{
                     mcb(null,comment);
                 });
             },(err,comments)=>{
-                
+                //console.log('comments:',err,comments);
+                comments=comments.filter((comment)=>{return comment;});
+                cb(null,post,comments);
             });
         }
-    ],(err)=>{
+    ],(err,post,comments)=>{
+        //console.log('post ret:',err,post);
+        //console.log('comments ret:',err,comments);
         if(err)return next(err);
         res.render('article',{
             title:'博客详细页',
-            post:ret,
+            post:post,
+            comments:comments,
             user:req.session.user,
-            op:req.session.user&&req.session.user.id==ret.userId,
+            op:req.session.user&&req.session.user.id==post.userId,
             success:req.flash('success').toString(),
             error:req.flash('error').toString(),
             refer:req.originalUrl
@@ -73,17 +95,18 @@ router.post('/:postId',checkLogin, function (req,res,next) {
     cli.hget('posts:'+postId,'id',(err,ret)=>{
         if(err)return next(err);
         if(!ret)return ep.emit('comment_err','文章不存在或已经删除');
-        //save
+
+        //save comment
         var id=uuid(),date=new Date(),time=moment(date).format('YYYY-MM-DD HH:mm:ss');
         async.parallel([
             (cb)=>{//评论数据
-                cli.hmset('comments:'+id,['comment',content,'userName',user.name,'userId',user.id,'time',time],(err)=>{
+                cli.hmset('comments:'+id,['comment',content,'userName',user.name,'time',time],(err)=>{
                     if(err)return cb(err);
                     cb();
                 });
             },
             (cb)=>{//以时间降序保存评论ID
-                cli.zadd('commentsIds:'+postId,[+date,id],(err)=>{
+                cli.zadd('postCommentIds:'+postId,[+date,id],(err)=>{
                     if(err)return cb(err);
                     cb();
                 });
